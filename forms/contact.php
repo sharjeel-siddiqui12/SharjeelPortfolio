6b4fcaf584cac7e85ddc7529fdceb9f95153c8b9
sharjeel-siddiqui12 <sharjeelsiddiqui00@gmail.com>
2026-01-21 03:37:47 +0500

    latest

--- forms/contact.php
+++ forms/contact.php
@@ -4,6 +4,8 @@
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
+
+    
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
   }
